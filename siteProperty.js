5c91f3126d8349b3500def3ec0f890659dfd84e0
Srinivas1130 <srinivasachar1998@gmail.com>
2022-06-27 12:56:21 +0530

    Added:code is refactor, ability to convert address book app to client server architecture

---
+++ siteProperty.js
@@ -0,0 +1,6 @@
+let site_properties = {
+    use_local_storage:"false",
+    home_page: "AddressBookHome.html",
+    add_emp_payroll_page: "AddressBookForm.html",
+    server_url: "http://127.0.0.1:3000/addressBook" 
+};
